0dd34f1e8b0788e5badbdc9aeeec99e7c086b2a7
Pawel Idzikowski <89011156+pawel-idzikowski-rmtcfm-com@users.noreply.github.com>
2022-08-26 13:41:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+// @NOTE invalid
+const something = require('src/test/something');
+const IndexController = require('src/controllers/employees/Index');
+const something2 = require('src/test/something2');
 
+// @NOTE valid
+const something = require('src/test/something');
+const something2 = require('src/test/something2');
+const IndexController = require('src/controllers/employees/Index');
+
+/*
+* comment without prefix
+*/
+
+/* @TODO
+* comment with prefix
+*/
+
+// @NOTE implicit index import
+const test1 = require('src/controllers/index');
+
+// @NOTE import with extension
+const test2 = require('src/helpers/doSomething.js');
+
+// @NOTE importing whole lodash lib
+const _ = require('lodash');
+
+module.exports = (value) => {
+  if (value > 5) console.log('not enough!');
+  
+  something(value).then((result) => {
+    console.log(result);
+  });
+  
+  
+  
+  console.log('a');
+  return 1;
+}
